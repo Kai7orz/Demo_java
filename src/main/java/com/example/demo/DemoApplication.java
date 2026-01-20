8181f113362c3ed9d3716b32e45794316ac82d1e
Kai7orz <kai7orz@gmail.com>
2026-01-20 14:33:20 +0900

    Java Function 型実装

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import com.example.demo.repository.MockTrainingRepository;
 import com.example.demo.repository.TrainingDataRepository;
 import com.example.demo.repository.TrainingRepository;
 import com.example.demo.service.TrainingService;
+import com.example.demo.usecase.user.Main;
 import com.example.demo.usecase.user.Monster;
+import com.example.demo.usecase.user.Pocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,26 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 @Configuration
 @ComponentScan
 public class DemoApplication {
 	public static void main(String[] args) {
-
-		Monster monster1 = new Monster("testName1");
-		Monster monster2 = new Monster("testName1");
-		Monster monster3 = new Monster("testName3");
-		Monster monster4 = new Monster("testName4");
-
-		List<Monster> monsterList1 = new ArrayList<>();
-		List<Monster> monsterList2 = new ArrayList<>();
-
-		System.out.println("Monster equal:"+ monster1.hashCode());
-		System.out.println("Monster equal:"+ monster2.hashCode());
-//		System.out.println("class information: "+DemoApplication.class);
-//		ApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
-//		// DI コンテナから任意の Bean を取得する
-//		TrainingService trainingService = context.getBean(TrainingService.class);
-//		trainingService.displayTrainingData();
+		// DataSaurce という Repository と JDBC の間をつなぐクラスを用意.
+		// DataSauce を JDBC を操作するためのクラスにインジェクションする.
+		Function<String,Integer> function = Main::len;
+		System.out.println(function.apply("hello"));
 	}
 }
